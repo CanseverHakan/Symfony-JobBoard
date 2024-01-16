2f36d2f18c549b4f3c5f2e86b9def1cfbdf968cd
Hakan Cansever <cansever.hakan94@gmail.com>
2024-01-16 10:46:16 +0100

    Form page

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Entrer votre adresse Email',
+                    'class' => 'mb-3'
+                ]
+            ])
+            ->add('username', TypeTextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Entrer votre nom d\'utilisateur',
+                    'class' => 'mb-3'
+                ]
+            ])
+            ->add('status', ChoiceType::class, [
+                'label' => false,
+                'choices' => [
+                    'Candidat' => 'Candidat',
+                    'Professionnel' => 'Professionnel'
+                ]
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'invalid_message' => 'Les mots de passe doivent être identiques.',
+                'options' => [
+                    'attr' => [
+                        'class' => 'password-field mb-3'
+                    ]
+                ],
+                'required' => true,
+                'first_options' => [
+                    'label' => false,
+                    'attr' => [
+                        'placeholder' => 'Entrez votre Mot de Passe',
+                        'class' => 'mb-3'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => false,
+                    'attr' => [
+                        'placeholder' => 'Confirmer votre Mot de Passe',
+                        'class' => 'mb-3'
+                    ]
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuiller entrer un mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ Limit }} caractères.',
+                        'max' => 4096,
+                    ])
+                ]
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'J\'accepte les condition d\'utilisation',
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez accepter les conditions d\'utilisation',
+                    ]),
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
